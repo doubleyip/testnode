4a1d98147f53e9d31d729d35515a90e7252630cf
doubleyip <syip428@gmail.com>
2017-11-06 12:37:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -42,6 +42,9 @@ app.get('/', function(req, res) {
   });
 });
 
+app.get('/', function(request, response) {
+  response.render('layouts/main');
+});
 app.listen(app.get('port'), function(){
 	console.log('Node app is running on port',app.get('port'));
 });
