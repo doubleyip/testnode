19506baac61ad47d5e96fb9a5d1b58212ba48ea0
doubleyip <syip428@gmail.com>
2017-11-06 12:47:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
+/*
 var express = require('express');
 var app = express();
-/*
 var exphbs = require('express-handlebars');
 var request = require('request');
 var async = require('async');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-*/
+
 
 app.set('port',(process.env.PORT || 3000));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'handlebars');
 console.log('TEST');
-/*
+
 app.get('/', function(req, res) {
   var data = {};
   var api_key = 'RGAPI-c16c2668-0913-4123-9416-113f700d30f0';
@@ -43,8 +43,28 @@ app.get('/', function(req, res) {
     });
   });
 });
-*/
+
 
 app.listen(app.get('port'), function(){
 	console.log('Node app is running on port',app.get('port'));
+});
+*/
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars');
+
+app.get('/', function(request, response) {
+  response.render('layouts/main');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
