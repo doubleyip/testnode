087ced2539789f25eef61bdafacd978efcda3adb
doubleyip <syip428@gmail.com>
2017-11-06 12:15:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 
 app.set('port',(process.env.PORT || 3000));
 app.set('view engine', 'handlebars');
-
+console.log('TEST');
 
 app.get('/', function(req, res) {
   var data = {};
